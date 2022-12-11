c57b7e9c437bb0c1ca22a3ba6927f5de14dba450
Caleb Moses <calebjdmoses@gmail.com>
2022-12-11 00:47:25 -0500

    Add dataloader test

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,13 +1,18 @@
+import pdb
 import torch
 from torch.utils.data import DataLoader
 from diffusion_lm.data import E2EDataset
 from diffusion_lm.model import DiffusionLM
 
+# Set up
 batch_size = 2
 diffusion_steps = 2000
 model = DiffusionLM()
 e2e_dataset = E2EDataset("train")
 e2e_dataloader = DataLoader(e2e_dataset, batch_size=batch_size, shuffle=True)
+
+def test_dataloader():
+    next(iter(e2e_dataloader)).shape[0] == batch_size
 
 def test_forward():
     batch = next(iter(e2e_dataloader))
@@ -16,7 +21,5 @@ def test_forward():
     noised_embeddings = model.q_sample(embeddings, timesteps)
     output = model.forward(noised_embeddings, timesteps)
 
-
-# def test_fit():
-#     e2e_dataset = load_dataset("e2e_nlg")
-#     model.fit(e2e_dataset["train"], epochs=1, batch_size=1)
+def test_fit():
+    model.fit([next(iter(e2e_dataloader))], epochs=1)
